1e0bc1aa22f6af5c24fc07a4d2a4500ebb0d242a
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-23 20:37:53 -0300

    Sass instalado e imagenes agregadas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Global.css';
 import App from './App';
+import './styles/Global.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
