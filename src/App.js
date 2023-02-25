ff60f93b32ca8b903f2ed06d89630dddc8d974f0
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-24 21:32:17 -0300

    Avances en navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles/App.scss';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Navbar.scss';
+
+export default function Navbar() {
+    return (
+        <header className='nav-main'>
+
+        </header>
+    )
+}
