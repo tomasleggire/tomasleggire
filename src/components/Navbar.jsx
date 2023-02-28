45b949d731eb811645713d8e2faef020ce07e966
Tomás Leggire <tomasleggire@hotmail.com>
2023-02-27 21:41:48 -0300

    Avances en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../styles/Navbar.scss';
+import { MdAccountBox } from "react-icons/md";
 
 export default function Navbar() {
     return (
         <header className='nav-main'>
-
+            <nav className='nav'>
+                <div className='name-title'>
+                    <MdAccountBox className='title-icon'/>
+                    <h3 className='title-h3'>tomás.me</h3>
+                </div>
+                <div className='nav-links'>
+                    <ul>
+                        <li>
+                            SERVICES
+                        </li>
+                        <li>
+                            PORTFOLIO
+                        </li>
+                        <li>
+                            PRICING
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
