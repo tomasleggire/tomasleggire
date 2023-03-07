6d0545636cfe592e4cc2e90f26bdf33001041c60
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-06 23:00:49 -0300

    Boton get started agregado

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -9,8 +9,11 @@ export default function Portada() {
             <h2 className="portada-h2">WEB APP DEVELOPMENT</h2>
             <p className="portada-p">I like to craft solid and scalable frontend products white great user experiences. My focus is on creating engaging, accessible & performant interfaces for humans.</p>
             <button type="button" className="portada-btn">
-                <MdOutlineArrowDownward className="portada-btn-icon" alt='Get Started'/>
+                <span className="btn-span-portada">Get Started</span>
             </button>
         </div>
     )
 }
+
+
+{/* <MdOutlineArrowDownward className="portada-btn-icon" alt='Get Started'/> */}
