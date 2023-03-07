141de639c8aed8747f0d99889afdcf80daa3e8ff
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-07 20:36:15 -0300

    Carrusel responsive

--- src/containers/Services.js
+++ src/containers/Services.js
@@ -3,36 +3,29 @@ import '../styles/Services.scss';
 import Slider from "react-slick";
 
 export default function Services() {
-    var settings = {
+    const settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
         responsive: [
           {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
+            breakpoint: 990,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
+              slidesToScroll: 1,
             }
           },
           {
-            breakpoint: 480,
+            breakpoint: 800,
             settings: {
               slidesToShow: 1,
-              slidesToScroll: 1
+              slidesToScroll: 1,
+              initialSlide: 0
             }
           }
         ]
