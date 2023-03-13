a71111047511b2f0935a4063975ff88da8cf0c02
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-13 18:37:41 -0300

    Services terminado

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/MoreInfo.scss';
 import ImageOne from '../images/web-design-layout.gif';
+import ImageTwo from '../images/web-designer-with-idea.gif';
 
 export default function MoreInfo() {
     return (
@@ -8,11 +9,17 @@ export default function MoreInfo() {
             <div className="moreInfo-one">
                 <img src={ImageOne} className='one-image'/>
                 <div className="one-texts">
-                  <h1 className="one-h1">MOBILE-FRIENDLY</h1>
+                  <h2 className="one-h2">MOBILE-FRIENDLY</h2>
                   <p className="one-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi facere recusandae qui aliquid molestiae reiciendis adipisci deleniti maiores, hic eos fugit amet veritatis ab possimus, tempora ex eaque nostrum.</p>
                 </div>
             </div>
-            <div className="moreInfo-two"></div>
+            <div className="moreInfo-two">
+                <img src={ImageTwo} className='two-image' />
+                <div className="two-texts">
+                  <h2 className="two-h2">MOBILE-FRIENDLY</h2>
+                  <p className="two-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi facere recusandae qui aliquid molestiae reiciendis adipisci deleniti maiores, hic eos fugit amet veritatis ab possimus, tempora ex eaque nostrum.</p>
+                </div>
+            </div>
         </div>
     )
 }
