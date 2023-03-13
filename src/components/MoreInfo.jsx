99b08337f0d287d0e07b4bd8bcedb310d6f90798
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-13 18:22:03 -0300

    moreInfo-one terminado

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import '../styles/MoreInfo.scss';
+import ImageOne from '../images/web-design-layout.gif';
 
 export default function MoreInfo() {
     return (
         <div className="moreInfo-main">
-            
+            <div className="moreInfo-one">
+                <img src={ImageOne} className='one-image'/>
+                <div className="one-texts">
+                  <h1 className="one-h1">MOBILE-FRIENDLY</h1>
+                  <p className="one-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi facere recusandae qui aliquid molestiae reiciendis adipisci deleniti maiores, hic eos fugit amet veritatis ab possimus, tempora ex eaque nostrum.</p>
+                </div>
+            </div>
+            <div className="moreInfo-two"></div>
         </div>
     )
 }
