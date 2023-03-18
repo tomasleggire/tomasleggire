000624d0a80f35598d3f9cb1d144d8adf8cc4203
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-18 20:49:43 -0300

    Hover animation en cards porfotlio

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -4,7 +4,7 @@ import '../styles/PortfolioItem.scss';
 export default function PortfolioItem({ image, title, text }) {
     return (
         <div className="card-item">
-            <img src={image} className='image-item'/>
+            <img src={image} className='image-item' alt={title}/>
             <h2 className="item-h1">{title}</h2>
             <span className="item-text">{text}</span>
         </div>
