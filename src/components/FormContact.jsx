753337a47290484181ac56afa982a7f7bf8b458e
Tomás Leggire <tomasleggire@hotmail.com>
2023-03-30 19:37:58 -0300

    Implemented form validation functionality

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,20 +1,46 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import '../styles/FormContact.scss';
 
 export default function FormContact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
 
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
+        if (name.trim() === '') {
+            alert('Please enter your name');
+            return;
+        }
+
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            alert('Please enter a valid email address');
+            return;
+        }
+
+        if (message.trim() === '' || message.length > 500) {
+            alert('Please enter a message (maximum 500 characters)');
+            return;
+        }
+
         emailjs.sendForm('service_8t7h4xr', 'template_6sadc4u', form.current, 'pWAZtesL7h2wPSXk0')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
-                console.log(error.text);
+                alert(`Somethig it's wrong, try again later`);
             });
+
+        setName('');
+        setEmail('');
+        setMessage('');
+
+        alert('Email enviado');
     };
 
     return (
@@ -24,16 +50,16 @@ export default function FormContact() {
                 <div className='form-names'>
                     <div className='form-names-name'>
                         <label className='label'>NAME</label>
-                        <input type='text' name="user_name" className='input-name' required/>
+                        <input type='text' name="user_name" className='input-name' value={name} onChange={(e) => setName(e.target.value)}/>
                     </div>
                     <div className='form-names-email'>
                         <label className='label'>EMAIL</label>
-                        <input type='email' name="user_email" className='input-email' required/>
+                        <input type='email' name="user_email" className='input-email' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                 </div>
                 <div className='form-message'>
                     <label className='label'>MESSAGE</label>
-                    <textarea name="message" className='form-msg-input' required/>
+                    <textarea name="message" className='form-msg-input' value={message} onChange={(e) => setMessage(e.target.value)}/>
                 </div>
                 <input type="submit" value="Send" className='btn-send' />
             </form>
