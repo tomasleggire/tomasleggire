1394341d3da47eb2ae84122d5c0d2aca4345e6e2
Tomás Leggire <tomasleggire@hotmail.com>
2023-04-11 11:24:57 -0300

    slider terminado

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -4,7 +4,7 @@ import '../styles/Option.scss';
 export default function Option({image, text, title}) {
     return (
         <div className="option-main">
-            <div>
+            <div className="option-image-div">
                 <img src={image} className="option-imagen"/>
             </div>
             <h6 className="option-title">{title}</h6>
