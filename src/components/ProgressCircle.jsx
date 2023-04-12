81f9f1a858eb2f2d6ffbcbac0930f8fad0f808bd
Tomás Leggire <tomasleggire@hotmail.com>
2023-04-12 12:08:19 -0300

    circles terminados

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -6,13 +6,13 @@ import AnimatedProgressProvider from '../hooks/AnimatedProgressProvider';
 import { easeQuadInOut } from "d3-ease";
 
 
-export default function ProgressCircle({ }) {
+export default function ProgressCircle({ porcentaje, name }) {
     return (
         <div className="circle-main-div">
             <div className="circle-progress">
                 <AnimatedProgressProvider
                     valueStart={0}
-                    valueEnd={66}
+                    valueEnd={porcentaje}
                     duration={2.0}
                     easingFunction={easeQuadInOut}
                 >
@@ -21,13 +21,21 @@ export default function ProgressCircle({ }) {
                         return (
                             <CircularProgressbar
                                 value={value}
+                                strokeWidth={10}
                                 text={`${roundedValue}%`}
-                                styles={buildStyles({ pathTransition: "none" })}
+                                styles={buildStyles({
+                                     pathTransition: "none",
+                                     textColor: 'lightgray',
+                                     pathColor: '#edd32d',
+                                     trailColor: '#b6b094',
+                                     textSize: '20px',
+                                })}
                             />
                         );
                     }}
                 </AnimatedProgressProvider>
             </div>
+            <h1 className="h1-progress-circle">{name}</h1>
         </div>
     )
 }
--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,13 +1,17 @@
 import React from "react";
-import '../styles/Resume.scss';
+import "../styles/Resume.scss";
 import Title from "../components/Title";
 import ProgressCircle from "../components/ProgressCircle";
 
 export default function Resume() {
-    return (
-        <div className="resume-main-div">
-            <Title title='MY SKILLS' subTitle='SKILLS' />
-            <ProgressCircle />
-        </div>
-    )
+  return (
+    <div className="resume-main-div">
+      <Title title="MY SKILLS" subTitle="SKILLS" />
+      <div className="main-circles">
+        <ProgressCircle porcentaje={95} name="Front-end Development" className='circle-1'/>
+        <ProgressCircle porcentaje={75} name="Mobile App Development" className='circle-2'/>
+        <ProgressCircle porcentaje={30} name="Back-end Development" className='circle-3'/>
+      </div>
+    </div>
+  );
 }
