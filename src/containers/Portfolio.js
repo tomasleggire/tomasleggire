c4f62453673bb7f64633991fbb69b576e6e785e9
Tomás Leggire <tomasleggire@hotmail.com>
2023-04-15 21:31:05 -0300

    portfolio items ajustados

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -8,48 +8,47 @@ import Title from "../components/Title";
 export default function Portfolio() {
   const { portfolioItems, mainItemModalPort, changeMainModalItem } =
     useContext(AppContext);
-
   const [modalItemValue, setModalItemValue] = useState(false);
-
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 500)
-  }, [])
+    }, 500);
+  }, []);
 
   return (
     <div className="portfolio-main">
       <Title title="MY WORKS" subTitle="WORKS" />
-      {loading ? 
-        <p>Cargando...</p> : 
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
         <>
-      <div className="container-portfolio">
-        <div className="cards">
-          {portfolioItems.map((item) => {
-            return (
-              <PortfolioItem
-                id={item.id}
-                image={item.image}
-                title={item.title}
-                text={item.text}
-                setModalItemValue={setModalItemValue}
-                changeMainModalItem={changeMainModalItem}
-              />
-            );
-          })}
-        </div>
-      </div>
-      {modalItemValue && (
-        <ModalItemPort
-          setModalItemValue={setModalItemValue}
-          mainItemModalPort={mainItemModalPort}
-        />
+          <div className="container-portfolio">
+            <div className="cards">
+              {portfolioItems.map((item) => {
+                return (
+                  <PortfolioItem
+                    id={item.id}
+                    image={item.image}
+                    title={item.title}
+                    text={item.text}
+                    setModalItemValue={setModalItemValue}
+                    changeMainModalItem={changeMainModalItem}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          {modalItemValue && (
+            <ModalItemPort
+              setModalItemValue={setModalItemValue}
+              mainItemModalPort={mainItemModalPort}
+            />
+          )}
+        </>
       )}
-      </>
-      }
     </div>
   );
 }
