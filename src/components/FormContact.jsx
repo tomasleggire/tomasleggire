6129a62c4a38bf082273569104edb733fad482d4
Tomás Leggire <tomasleggire@hotmail.com>
2023-04-28 22:44:17 -0300

    contactForm terminado con manejo de errores

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import '../styles/FormContact.scss';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function FormContact() {
 
@@ -15,17 +16,17 @@ export default function FormContact() {
         e.preventDefault();
 
         if (name.trim() === '') {
-            alert('Please enter your name puto');
+            toast.error("Enter a valid name", { duration: 2500 });
             return;
         }
 
         if (!/\S+@\S+\.\S+/.test(email)) {
-            alert('Please enter a valid email address xd');
+            toast.error("Enter a valid email address", { duration: 2500 });
             return;
         }
 
         if (message.trim() === '' || message.length > 500) {
-            alert('Please enter a message (maximum 500 characters)');
+            toast.error("Please enter a message (maximum 500 characters)", { duration: 2500 });
             return;
         }
 
@@ -33,32 +34,34 @@ export default function FormContact() {
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
-                alert(`Somethig it's wrong, try again later`);
+                toast.error("Somethig it's wrong, try again later", { duration: 2500 });
+
             });
 
         setName('');
         setEmail('');
         setMessage('');
 
-        alert('Your email has been sent successfully!');
+        toast.success("Your email has been sent successfully!", { duration: 2500 });
     };
 
     return (
         <div className="main-form">
             <form ref={form} onSubmit={sendEmail} className='form-contact'>
-                    <div className='form-names-name'>
-                        <label className='label'>NAME</label>
-                        <input type='text' name="user_name" className='input-name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name...'/>
-                    </div>
-                    <div className='form-names-email'>
-                        <label className='label'>EMAIL</label>
-                        <input type='email' name="user_email" className='input-email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email...'/>
-                    </div>
+                <div className='form-names-name'>
+                    <label className='label'>NAME</label>
+                    <input type='text' name="user_name" className='input-name' value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter your name...' />
+                </div>
+                <div className='form-names-email'>
+                    <label className='label'>EMAIL</label>
+                    <input type='email' name="user_email" className='input-email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter your email...' />
+                </div>
                 <div className='form-message'>
                     <label className='label'>MESSAGE</label>
-                    <textarea name="message" className='form-msg-input' value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Enter you message...'/>
+                    <textarea name="message" className='form-msg-input' value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Enter you message...' />
                 </div>
                 <button type="submit" className='btn-send'>SEND EMAIL</button>
+                <Toaster position='top-center' />
             </form>
         </div>
     )
